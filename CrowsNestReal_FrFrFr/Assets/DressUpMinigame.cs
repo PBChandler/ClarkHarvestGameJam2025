5ed96ca314cfe9ee0a5325d076a4fd8e3b5ff486
Lhetzel <lvalentinehetzel@clarku.edu>
2025-09-20 16:05:09 -0400

    dressup game works
    luke art assets

--- CrowsNestReal_FrFrFr/Assets/DressUpMinigame.cs
+++ CrowsNestReal_FrFrFr/Assets/DressUpMinigame.cs
@@ -4,31 +4,5 @@ using UnityEngine.UI;
 
 public class DressUpMinigame : Minigame
 {
-    public Sprite[] spriteArray;
-    int index = 0;
-    public Image currentImage;
-
-    public void displayImage()
-    {
-        if (index >= 0 && index < spriteArray.Length)
-        {
-            currentImage.sprite = spriteArray[index];
-        }
-        else
-        {
-            Debug.LogWarning("Invalid image index: " + index);
-        }
-    }
-
-    public void nextImage()
-    {
-        index = (index+1)%spriteArray.Length;
-        displayImage();
-    }
-
-    public void prevImage()
-    {
-        index = (index-1+spriteArray.Length)%spriteArray.Length;
-        displayImage();
-    }
+   
 }
---
+++ CrowsNestReal_FrFrFr/Assets/dressupArrow.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class dressupArrow : MonoBehaviour
+{
+    public UnityEvent myEvent;
+    public Sprite[] spriteArray;
+    int index = 0;
+    public SpriteRenderer currentImage;
+
+    private void OnMouseDown()
+    {
+        myEvent.Invoke();
+    }
+
+    public void displayImage()
+    {
+        if (index >= 0 && index < spriteArray.Length)
+        {
+            currentImage.sprite = spriteArray[index];
+        }
+        else
+        {
+            Debug.LogWarning("Invalid image index: " + index);
+        }
+    }
+
+    public void nextImage()
+    {
+        index = (index + 1) % spriteArray.Length;
+        displayImage();
+    }
+
+    public void prevImage()
+    {
+        index = (index - 1 + spriteArray.Length) % spriteArray.Length;
+        displayImage();
+    }
+
+    public void testButton()
+    {
+        Debug.Log("my princess is so beautiful..");
+    }
+}
