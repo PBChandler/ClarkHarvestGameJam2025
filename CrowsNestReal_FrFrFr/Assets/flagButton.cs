36fceef0a8cbc9544371571b42cb8fc6e1e8e94d
Stump <LStump@clarku.edu>
2025-09-20 16:37:18 -0400

    Flags! (mockup)
    yay wahoo I love flag minigame :)

---
+++ CrowsNestReal_FrFrFr/Assets/flagButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class flagButton : MonoBehaviour
+{
+    public Image image;
+    public Sprite flagSprite;
+    public Sprite iconSprite;
+    Image iconImage;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        iconImage = gameObject.transform.GetChild(0).gameObject.GetComponent<Image>();
+        iconImage.sprite = iconSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setImage()
+    {
+        image.sprite = flagSprite;
+    }
+}
