e31008f06b130d2c0cdfd51ba7c98e095d471c69
Stump <LStump@clarku.edu>
2025-09-20 17:10:27 -0400

    Startup :)
    Hobby Horse :))

---
+++ CrowsNestReal_FrFrFr/Assets/Startup.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Startup : MonoBehaviour
+{
+    public float delay;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Invoke("LoadNextScene", delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
