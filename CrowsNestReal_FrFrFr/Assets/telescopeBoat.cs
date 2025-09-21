7ebd0cf80a98cc0e6d9b72d56f5cfd3e6cb213a8
Stump <LStump@clarku.edu>
2025-09-21 14:50:07 -0400

    made dialogue and boats work in telescope
    clown boat is still weird

--- CrowsNestReal_FrFrFr/Assets/telescopeBoat.cs
+++ CrowsNestReal_FrFrFr/Assets/telescopeBoat.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using TMPro;
 
 public class telescopeBoat : MonoBehaviour
 {
     public GameObject panel;
     public float position;
     public GameObject backButton;
+    public Sprite[] ships;
+    public string[] dialogues;
+    public TextMeshProUGUI textBox;
+    private Sprite ship;
+    private SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         panel.SetActive(false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("wait", 0.1f);
     }
 
+    void wait()
+    {
+        int i = 0;
+        switch (DATADUDE.instance.SHIP)
+        {
+            case GameManager.activeShipTarget.NULL:
+                break;
+            case GameManager.activeShipTarget.Navy:
+                i = 0;
+                break;
+            case GameManager.activeShipTarget.Ghost:
+                i = 1;
+                break;
+            case GameManager.activeShipTarget.Clowns:
+                i = 2;
+                break;
+            default:
+                break;
+        }
+        ship = ships[i];
+        textBox.text = dialogues[i];
+    }
     // Update is called once per frame
     void Update()
     {
@@ -21,5 +51,11 @@ public class telescopeBoat : MonoBehaviour
     {
         panel.SetActive(true);
         backButton.SetActive(true);
+        float scale = 0.5f;
+        transform.localScale = new Vector3(scale,scale,1);
+        Vector3 pos = transform.localPosition;
+        pos.y = 0;
+        transform.localPosition = pos;
+        spriteRenderer.sprite = ship;
     }
 }
